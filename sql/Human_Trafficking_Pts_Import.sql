938b5e4aad7a6dc407cce619dc6cfa64b4b0e2b3
brodydittemore-eDip <dittemoremb@state.com>
2014-04-03 13:06:46 -0400

    Adding Human Trafficking Pts

---
+++ sql/Human_Trafficking_Pts_Import.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS public."Human_Trafficking_Pts";
+CREATE TABLE public."Human_Trafficking_Pts"
+(
+	"Header" text,
+	"Topic" text,
+	"Map" text,
+	"Country" text,
+	"Title" text,
+	"Story" text,
+	"Lat" character varying(15),
+	"Long" character varying(15)
+);
+
+set client_encoding to 'UTF-8';
+
+COPY "Human_Trafficking_Pts" FROM 'C:\OpenGeo\webapps\DiplomacyExplorer2\sql\Human_Trafficking_Pts.csv' DELIMITER ',' CSV;
+
+SELECT AddGeometryColumn( 'public', 'Human_Trafficking_Pts', 'the_geom', 4326, 'POINT', 2 );
+
+UPDATE public."Human_Trafficking_Pts" SET the_geom = ST_SetSRID(ST_GeomFromText('POINT(' || "Long" || ' ' || "Lat" || ')'),4326);
