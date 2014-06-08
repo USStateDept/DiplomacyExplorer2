aa4e21c518c7c7365ad21ef2ff46aceb55710f2d
bryant-edip <bryantlc@state.gov>
2014-06-08 12:24:55 -0400

    Updated Admin.sql script

--- sql/Admin.sql
+++ sql/Admin.sql
@@ -1,4 +1,4 @@
-create SEQUENCE "THEME_fid_seq"
+create SEQUENCE "THEME_fid_seq";
 
 CREATE TABLE "THEME"
 (
@@ -10,7 +10,7 @@ CONSTRAINT "THEME_pkey" PRIMARY KEY (fid )
 
 COPY "THEME" FROM 'C:\OpenGeo\webapps\DiplomacyExplorer2\sql\issue.csv' DELIMITER ',' CSV;
 
-create SEQUENCE "ISSUE_fid_seq"
+create SEQUENCE "ISSUE_fid_seq";
 
 CREATE TABLE "ISSUE"
 (
@@ -19,5 +19,5 @@ fid integer NOT NULL DEFAULT nextval('public."ISSUE_fid_seq"'::regclass),
 "Legend" text,
 "Theme_id" integer DEFAULT NULL,
 FOREIGN KEY ("Theme_id") REFERENCES "THEME"("fid") on delete cascade on update cascade,
-CONSTRAINT "THEME_pkey" PRIMARY KEY (fid )
+CONSTRAINT "ISSUE_pkey" PRIMARY KEY (fid )
 );
