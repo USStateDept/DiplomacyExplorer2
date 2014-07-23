2087ab114364f6813dfea9e7bf589bf708b8635a
nathanhilbert <nathanhilbert@gmail.com>
2014-07-23 16:45:01 -0400

    updated urls to take care of case

--- dipex/dipex/urls.py
+++ dipex/dipex/urls.py
@@ -10,15 +10,16 @@ import layerinfo
 urlpatterns = patterns('',
     # Examples:
     #url(r'^' + settings.BASE_URL + '$', 'layerinfo.views.home', name='home'),
-    (r'^' + settings.BASE_URL + '$', RedirectView.as_view(url='/' + settings.BASE_URL + '/')),
     url(r'^' + settings.BASE_URL + '/$', 'layerinfo.views.home', name='home'),
-    url(r'^' + settings.BASE_URL + '/proxy$', 'layerinfo.views.proxy', name='proxy'),
-    url(r'^' + settings.BASE_URL + '/geojson$', 'layerinfo.views.geoJson', name='proxy'),
+    (r'^(?i)' + settings.BASE_URL + '$', RedirectView.as_view(url='/' + settings.BASE_URL + '/')),
+    (r'^(?i)' + settings.BASE_URL + '/$', RedirectView.as_view(url='/' + settings.BASE_URL + '/')),
+    url(r'^(?i)' + settings.BASE_URL + '/proxy$', 'layerinfo.views.proxy', name='proxy'),
+    url(r'^(?i)' + settings.BASE_URL + '/geojson$', 'layerinfo.views.geoJson', name='proxy'),
     #url(r'^' + settings.BASE_URL + '/Combo$', 'layerinfo.views.combobox', name='proxy'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^' + settings.BASE_URL + '/layerinfo', include('layerinfo.urls')),
-    url(r'^' + settings.BASE_URL + '/admin/', include(admin.site.urls)),
-    (r'^' + settings.BASE_URL + '/tinymce/', include('tinymce.urls')),
+    url(r'^(?i)' + settings.BASE_URL + '/layerinfo', include('layerinfo.urls')),
+    url(r'^(?i)' + settings.BASE_URL + '/admin/', include(admin.site.urls)),
+    (r'^(?i)' + settings.BASE_URL + '/tinymce/', include('tinymce.urls')),
 )
 
 
@@ -28,5 +29,5 @@ from django.conf import settings
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
     urlpatterns += patterns('',
-        (r'^' + settings.BASE_URL + '/media/(?P<path>.*)$', 'django.views.static.serve', {
+        (r'^(?i)' + settings.BASE_URL + '/media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT}))
