3043d959c61c6b18e87c69c7b6e59d3e78bbd392
nathanhilbert <nathanhilbert@gmail.com>
2014-08-05 11:28:28 -0400

    closes #62 until we get further instructions

--- dipex/layerinfo/static/js/webtour.js
+++ dipex/layerinfo/static/js/webtour.js
@@ -1,50 +1,97 @@
 // Instance the tour
+var tourduration = 4000;
 var tour = new Tour({
   storage:false,
   steps: [
   {
-    element: "#business",
-    title: "Hereis a nice menu",
-    content: "Content of my step",
+    element: "",
+    orphan: true,
+    title: "Welcome",
+    content: "Welcome to the Diplomacy Explorer Tour.  You will see some features of the application and learn a little about Diplomacy along the way.",
+    duration: tourduration,
     onShow: function (tour) {}
     //backdrop:true
   },
   {
     element: "#business",
-    title: "There are lots of sublayers",
-    content: "Content of my step",
+    title: "Menu",
+    content: "This the main menu.  It is split into five themes of diplomacy.",
+    duration: tourduration,
+    //onStart: function(tour) {$("#businessDrop").trigger('click');},
+    onShown: function (tour) {}
+  },
+  {
+    element: "#business",
+    title: "Issues",
+    content: "Each of these themes have a number of issues associated to them.",
+    duration: tourduration,
     //onStart: function(tour) {$("#businessDrop").trigger('click');},
     onShown: function (tour) {$("#business").trigger('click');}
   },
   {
     element: "#business",
-    title: "Let's click one of these",
-    content: "notice the side menu now shows",
+    title: "For example.",
+    content: "For example, diplomacy can be a tool for building businesses through issuing visas and supporting tourism.  Let's click on the Visa and Tourism issue to explore further.",
+    duration: tourduration,
     //onStart: function(tour) {$("#businessDrop").trigger('click');},
-    onShown: function (tour) {$("[name='NuclearArmsControlKey']").trigger('click');}
+    onShown: function (tour) {$("#business").trigger('click');}
   },
   {
-    element: "#geoJsonLayerMLOOECDid",
-    title: "Let's look at a sublayer",
-    content: "notice the side menu now shows",
+    element: "#mapKey",
+    title: "",
+    content: "You can read about the issue at the top.  Below are a number a maps that visually represent the issue.  Let's click on one.",
+    duration: tourduration,
     //onStart: function(tour) {$("#businessDrop").trigger('click');},
-    onShown: function (tour) {$("#NNPTDeposited_2014_Key").trigger('click');}
-  },
-  {
-    element: "",
-    orphan: true,
-    title: "I can even zoome the map if we want",
-    content: "notice the side menu now shows",
-    //onStart: function(tour) {$("#businessDrop").trigger('click');},
-    onShown: function (tour) {map.zoomIn();}
+    onShown: function (tour) {$("[name=VisasAndTourism_Key]").trigger('click');}
   },
   {
     element: "",
+    title: "",
     orphan: true,
-    title: "and pan to specific places",
-    content: "notice the side menu now shows",
+    content: "You will notice a lot has changed on the map.  Let's explore each part.",
+    duration: tourduration,
     //onStart: function(tour) {$("#businessDrop").trigger('click');},
-    onShown: function (tour) {map.panTo([39,-37.698]);map.setZoom(6)} 
+    onShown: function (tour) {$("#NIV_1997_Keyid").trigger('click');} 
+    //onShown: function (tour) {map.panTo([39,-37.698]);map.setZoom(6)} 
+  },
+  {
+    element: ".legend",
+    title: "",
+    content: "The colors have changed on the countries.  Keep an eye on the legend to understand what they are representing.",
+    duration: tourduration,
+    //onStart: function(tour) {$("#businessDrop").trigger('click');},
+    onShown: function (tour) {} 
+    //onShown: function (tour) {map.panTo([39,-37.698]);map.setZoom(6)} 
+  },
+  {
+    element: "",
+    title: "",
+    orphan: true,
+    content: "Some maps have stories associated to them.  Click on the flag will open the story.",
+    duration: tourduration,
+    //onStart: function(tour) {$("#businessDrop").trigger('click');},
+    onShown: function (tour) {map.panTo([10,10]);map.setZoom(5);$(".leaflet-marker-icon").first().trigger('click');} 
+    //onShown: function (tour) {map.panTo([39,-37.698]);map.setZoom(6)} 
+  },
+  {
+    element: "",
+    title: "",
+    orphan: true,
+    content: "Sometimes it is hard to see the smaller countries.  Zoom in with your mousewheel.",
+    duration: tourduration,
+    //onStart: function(tour) {$("#businessDrop").trigger('click');},
+    onShown: function (tour) {map.panTo([30,30]);map.setZoom(5)} 
+    //onShown: function (tour) {map.panTo([39,-37.698]);map.setZoom(6)} 
+  },
+  {
+    element: ".timeSlider",
+    title: "",
+    placement: "left",
+    content: "There are special features for layers that change over time. Slide the bar to see the number of visas issues for each year. ",
+    duration: tourduration,
+    //onStart: function(tour) {$("#businessDrop").trigger('click');},
+    onShown: function (tour) {} 
+    //onShown: function (tour) {map.panTo([39,-37.698]);map.setZoom(6)} 
   }
   
 ]});
